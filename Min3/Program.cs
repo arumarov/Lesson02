e0dfb864afe06f03e301d39d756ed02c666d67da
Arumarov <arumarov@yandex.ru>
2021-11-01 23:10:52 +0600

    Add Min3

---
+++ Min3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Нужно найти минимальное из 3-х чисел
+int min (int a, int b, int c) // пишем функцию min - нахождение минимального числа
+{
+       int result = a;
+       if (b < result) result = b;
+       if (c < result) result = c;
+       return result;
+    
+}
+
+Console.WriteLine(min (12, 5, 88));  // обращаемся к подпрограмме min
